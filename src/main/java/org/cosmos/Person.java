3798bf034945b37c040924855afc7bbc1cf97731
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2024-11-29 15:55:17 +0100

    created getInfos method

--- src/main/java/org/cosmos/Person.java
+++ src/main/java/org/cosmos/Person.java
@@ -1,4 +1,17 @@
 package org.cosmos;
 
+import java.util.Random;
+
 public class Person {
+    Random rand= new Random();
+    private int id;
+    private String name;
+    private String Address;
+    private int contact;
+
+
+
+    public String getInfos(){
+        return"Id: "+id+"   Name: "+name+"  Address: "+Address+"    Contact: "+contact;
+    }
 }
