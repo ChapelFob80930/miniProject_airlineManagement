30edb14c65081259d935cc5e38a17c9e25011199
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2024-11-29 16:12:46 +0100

    Aircraft, Airport, Book, Flight, Person classes structure created

--- src/main/java/org/cosmos/Aircraft.java
+++ src/main/java/org/cosmos/Aircraft.java
@@ -1,4 +1,12 @@
 package org.cosmos;
 
 public class Aircraft {
+    private int Registration;
+    private String Model;
+    private int Capacity;
+
+    public void assignFLight(){};
+    public void checkAvailability(){};
+
+
 }
