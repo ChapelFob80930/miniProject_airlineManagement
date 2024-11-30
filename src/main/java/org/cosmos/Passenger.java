6ff1023a9ffc2340017f0c007b3051203fb5cf81
Thrinath008 <148782730+Thrinath008@users.noreply.github.com>
2024-11-30 17:33:58 +0100

    modified bookflight in passanger class

--- src/main/java/org/cosmos/Passenger.java
+++ src/main/java/org/cosmos/Passenger.java
@@ -1,17 +1,40 @@
 package org.cosmos;
 
+import java.util.Scanner;
+
 public class Passenger extends Person{
-    private int Passport;
+    private String Passport;
 
 
-    public void bookFlight(){
+    public static void bookFlight(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your passport number: ");
+        String passport = scanner.next();
+        System.out.println("Enter the place your traveling from: ");
+        String departure = scanner.next();
+        System.out.println("Enter your destination: ");
+        String destination = scanner.next();
+        System.out.println("Enter the date of traveling: ");// will adjust the format of the date later;
+        String dateofjourney = scanner.next();
+        System.out.println("Enter the number of travellers: ");
+        int numberoftravellers = scanner.nextInt();
+
+
+
 
     }
+
+
     public void cancelBook(int BookingNumber){
+
         Reservations.cancelReservation(BookingNumber);
     }
 
-    public static Passenger getInstance() {
-        return null;// prints that he booked or canceled the flight.....
+    public String getBooking() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your reservation ID: ");
+        int ID = scanner.nextInt();
+        // if id in the regID return booking details;
+        return null;
     }
 }
